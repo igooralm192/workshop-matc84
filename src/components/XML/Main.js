892b6e6f705f5cba9bbd3443109e8cda290474a6
gustavo4passos <gustavo4passos@gmail.com>
2019-09-21 20:11:27 -0300

    Adds editor to XML main page

--- src/components/XML/Main.js
+++ src/components/XML/Main.js
@@ -5,12 +5,17 @@ import Editor from '../Editor'
 
 const styles = theme => ({});
 
+let editorStatus = "<rect x=\"30\" y=\"25\" w=\"50\" h=\"50\"></rect>";
+let onChange = (value) => { editorStatus = value; }
+
 function Main(props) {
 	const { classes } = props;
     return (
 		<div style={ { height:"100%" } }>
 			<Editor 
-				mode="xml"
+				mode= "xml"
+				value = {editorStatus}
+				onChange = {onChange}
 			/>
 		</div>
     )
