a2cd4a61858bc15c292b46d8b57ff832f1b5f9a3
gustavo4passos <gustavo4passos@gmail.com>
2019-09-23 01:19:21 -0300

    Initial commit

---
+++ src/components/XML/XmlParser.js
@@ -0,0 +1,25 @@
+var xmltojson = require('xml-js')
+
+class ParseError {
+	constructor(errorMessage) {
+		this.errorMessage = errorMessage; 
+	}
+
+	ErrorMessage() {
+ 		return "[Error] " + this.errorMessage;
+	}
+};
+
+function parse(xml) { 
+	let json;
+	try {
+		json = xmltojson.xml2json(xml, {compact: true, spaces: 4 });
+	} catch(e) {
+		return new ParseError(e.message);
+	}
+	
+	const obj  = JSON.parse(json);
+	return obj;
+}
+
+export default parse;
