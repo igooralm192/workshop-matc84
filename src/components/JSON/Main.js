acebf7598b34ed59f076ceea2979eec1c3ced4bc
Igor de Almeida <igoor1205@gmail.com>
2019-09-28 15:53:55 -0300

    Add on change step functions

--- src/components/JSON/Main.js
+++ src/components/JSON/Main.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Challenge from '../Challenge';
 import Editor from '../Editor';
 
 const styles = theme => ({});
 
+const challenge = "Space Invaders";
+
+const steps = [
+    {
+        title: challenge,
+        subtitle: 'Introdução',
+        description: 'Vamos iniciar ae krl',
+        initProperties: {}
+    },
+    {
+        title: challenge,
+        subtitle: 'Background',
+        description: 'Faz o fundo ae poha',
+        initProperties: {}
+    }
+]
+
 function Main(props) {
+    const [step, setStep] = useState(0);
 	const { classes } = props;
-	
+	console.log(step)
     return (
         <div style={{height: '100%'}}>
             <Challenge 
@@ -15,6 +33,10 @@ function Main(props) {
                 subtitle={'Titulo da etapa'} 
                 description={'Descrição da etapa'} 
                 editor={<Editor mode="json"/>}
+                steps={steps}
+                activeStep={step}
+                previousStep={() => setStep(Math.max(0, step-1))}
+                nextStep={() => setStep(Math.min(steps.length-1, step+1))}
             />
         </div>
     )
