6398c98a7c1f8d00d0a02340b3d56829e501d969
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2019-09-29 18:56:47 -0300

    Add steps description

--- src/components/JSON/SpaceInvaders/Main.js
+++ src/components/JSON/SpaceInvaders/Main.js
@@ -10,19 +10,69 @@ const steps = [
     {
         title: challenge,
         subtitle: 'Introdução',
-        description: 'Vamos iniciar ae krl',
+        description: <p>Bem vindo(a) ao desafio JSON do workshop da disciplina MATC84! Aqui, iremos aprender o básico da linguagem através da modificação de certas propriedades do jogo Space Invaders. <p>Atente-se às instruções e apenas avance quando completa-las! Por hora, pode seguir adiante!</p> </p>,
         initProperties: {}
     },
     {
         title: challenge,
         subtitle: 'Background',
-        description: 'Vamos iniciar ae krl',
+        description: <p>Se queremos lutar no espaço sideral, é claro que devemos estar nele primeiro. Para isso, insira na raiz do código JSON um objeto chamado background com os seguintes atributos:
+        <ul>
+            <li>color - (recebendo uma cor) para a cor do fundo</li>
+            <li>starsNumber - (inteiro) para a quantidade de estrelas</li>
+            <li>starsColor - (cor) para a cor das estrelas</li>
+        </ul>
+        <p>Para isso, saiba que uma cor é uma string com o nome da cor ou um número em hexadecimal ou rgb(x, y, z) com x, y e z sendo os niveis de vermelho, verde e azul de 0 a 255.</p>
+        </p>,
         initProperties: {}
     },
     {
         title: challenge,
         subtitle: 'Invaders',
-        description: 'Vamos iniciar ae krl',
+        description: <p>Agora, preparemo-nos para encontrar nossos inimigos! Assim, insira na raiz do código JSON um objeto chamado invader com os seguintes atributos:
+        <ul>
+            <li>amount - (inteiro) para a quantidade dos Invaders</li>
+            <li>lives - (inteiro) para definir a vida dos inimigos</li>
+            <li>speed - (inteiro) para configurar a velocidade</li>
+            <li>acceleration - (inteiro) para configurar a aceleração</li>
+            <li>bombColor - (cor) para definir a cor da bomba</li>
+            <li>bombRate - (float) para a taxa de bombas jogadas pelos Invaders</li>
+        </ul>            
+        </p>,
+        initProperties: {}
+    },
+    {
+        title: challenge,
+        subtitle: 'Nave',
+        description: <p>Enfim podemos construir nossa nave, capriche! Insira na raiz do código JSON um objeto chamado ship com os seguintes atributos:
+        <ul>
+            <li>width - (inteiro) para a largura da nave</li>
+            <li>height - (inteiro) para a altura da nave</li>
+            <li>speed - (inteiro) para configurar a velocidade</li>
+            <li>rocketVelocity - (inteiro) para definir a velocidade do tiro</li>
+            <li>shootRate - (float) para a taxa de tiros </li>
+        </ul>
+        </p>,
+        initProperties: {}
+    },
+    {
+        title: challenge,
+        subtitle: 'Toques finais',
+        description: <p>Falta pouco para jogar, mas precisamos de algumas configurações para iniciar. Insira na raiz do código JSON um objeto chamado game com os seguintes atributos:
+        <ul>
+            <li>lives - (inteiro) para definir a quantidade de vida do jogador</li>
+            <li>difficultyMultiplier - (float) para configurar a dificuldade do jogo</li>
+            <li>pointsPerInvader - (float) para a quantidade de pontos por inimigo destruído</li>
+        </ul>
+        </p>,
+        initProperties: {}
+    },
+    {
+        title: challenge,
+        subtitle: 'Hora de jogar',
+        description: <p>Parabéns! Você fez um ótimo trabalho, mas agora é hora de testar, não é mesmo? Insira na raiz do código JSON um atributo chamado end com o valor verdadeiro.
+            <p>Após isso, basta clicar no espaço do jogo e apertar espaço (space) para jogar. As setas para a esquerda e direita movem a sua nave e espaço (space) atira. Seu objetivo, claramente é destruir todos os Invaders e não morrer</p>
+        </p>,
         initProperties: {}
     }
 ]
