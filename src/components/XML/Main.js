417b377afee94df1e482e948f342e34f28338702
Alissonsz <alisson_tecsouza@hotmail.com>
2019-10-02 11:52:07 -0300

    just to pull

--- src/components/XML/Main.js
+++ src/components/XML/Main.js
@@ -9,6 +9,29 @@ import '../../assets/css/three.css'
 
 const styles = theme => ({});
 
+const challenge = 'XML'
+
+const steps = [
+    {
+        title: challenge,
+        subtitle: 'Introdução',
+        description: 'Vamos iniciar ae krl',
+        initProperties: {}
+    },
+    {
+        title: challenge,
+        subtitle: 'Background',
+        description: 'Vamos iniciar ae krl',
+        initProperties: {}
+    },
+    {
+        title: challenge,
+        subtitle: 'Invaders',
+        description: 'Vamos iniciar ae krl',
+        initProperties: {}
+    }
+]
+
 // Editor initial state
 let editorStatus = "<data>\n\t<rect x=\"30\" y=\"25\" w=\"50\" h=\"50\"/>\n</data>";
 
@@ -36,16 +59,18 @@ function Main(props) {
 	const { classes } = props;
     return (
 		<div style = { { height:"100%" } }>
-			<Challenge editor = {
-				<Editor 
-					mode= "xml"
-					value = {editorStatus}
-					onChange = {onChange}
-				/>
-			}
-			result = {
-				<MainThree/>
-			}>
+			<Challenge 
+				steps = {steps}
+				editor = {
+					<Editor 
+						mode= "xml"
+						value = {editorStatus}
+						onChange = {onChange}
+					/>
+				}
+				result = {
+					<MainThree/>
+				}>
 
 			</Challenge>
 			
