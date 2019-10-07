4d8d811a3559815a6a2ee74ea81917578b64e849
gustavo4passos <gustavo4passos@gmail.com>
2019-10-07 13:53:19 -0300

    Fixes introduction layout

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Grid, Paper } from '@material-ui/core' 
-import { Link        } from 'react-router-dom'
-import { withStyles  } from '@material-ui/core/styles';
+import { Grid, Paper, ListItem } from '@material-ui/core' 
+import { Link                  } from 'react-router-dom'
+import { withStyles  		   } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography'
 
 const styles = theme => ({});
@@ -10,18 +10,18 @@ function Introduction(props) {
 	const { classes } = props;
 	
     return (
-		<Grid container spacing={5} justify="space-evenly" alignItems="center" style={{minHeight: '500px'}}>
+		<Grid container spacing={5} justify="space-evenly" alignItems="center" style={{height: '100vh'}}>
 			<Grid item>
 				<Link to = { "/json/spaceinvaders" }>
-						<Typography variant="h2">
-							<span style={{color: "#FF9100" }}> {"</>"} </span> XML 
-						</Typography>
+					<Typography variant="h1">
+						<span style={{color: "#FF9100" }}> {"</>"} </span> XML 
+					</Typography>
 				</Link>
 			</Grid>
 
 			<Grid item>
 				<Link to = { "/xml/introduction" }>
-						<Typography variant="h2">
+						<Typography variant="h1">
 							<span style={{color: '#9F6BAB'}}> {"{;}"} </span> JSON 
 						</Typography>
 				</Link>
